419eb916fa4a5fcb6be660c9a773784bfa5cfd3c
Azeez <adenijiazeez@gmail.com>
2020-01-02 21:48:15 +0100

    Type: feat
    Exercise dir added
    
    Resolves: #1

---
+++ exercise/hello.py
@@ -0,0 +1,17 @@
+import os
+import base64
+import sys
+from io import BytesIO
+
+from flask import Flask, escape, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    name = request.args.get("name", "World")
+    return f'Hello, {escape(name)}!'
+
+$ env FLASK_APP=hello.py flask run
+ * Serving Flask app "hello"
+ * Running on http://127.0.0.1:5000/ (Press CTRL+C to quit)
